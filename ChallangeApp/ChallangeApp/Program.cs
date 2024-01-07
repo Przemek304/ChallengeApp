b7000441eeb0942479ebf5f6b2bbb588c7c9ac65
Admin <woronczukprzemek@gmail.com>
2024-01-07 13:16:27 +0100

    zadanie dzień 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,18 @@
-﻿    // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+var name = "Ania";
+var gender = "kobieta";
+var age = 29;
+
+if (gender == "kobieta" && age < 30)
+{
+    Console.WriteLine("kobieta poniżej 30lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa lat 30");
+}
+else if (gender == "meszczyzna" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni męszczyzna");
+}
