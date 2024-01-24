a922f531dce279ba17505ca567c98bdfe267d3a4
Admin <woronczukprzemek@gmail.com>
2024-01-24 22:27:52 +0100

    ćwiczenia

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,89 +1,28 @@
-﻿int number = 4594;
-string numberAsString = number.ToString();
-char[] letters = numberAsString.ToArray();
+﻿User user1 = new User("Adam", "12345");
+User user2 = new User("Monika", "12345");
+User user3 = new User("Zuzia", "12345");
+User user4 = new User("Damian", "12345");
 
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
 
-foreach (char letter in letters)
+user1.AddScore(5);
+
+class User
 {
+    private int score;
+  
 
-    if (letter == '0')
+    public User(string login, string password)
     {
-        counter0++;
+        this.Login = login;
+        this.Password = password;
+        this.score = 0;
     }
-    else if (letter == '1')
-    {
-        counter1++;
-    }
-    else if (letter == '2')
-    {
-        counter2++;
-    }
-    else if (letter == '3')
-    {
-        counter3++;
-    }
-    else if (letter == '4')
-    {
-        counter4++;
-    }
-    else if (letter == '5')
-    {
-        counter5++;
-    }
-    else if (letter == '6')
-    {
-        counter6++;
-    }
-    else if (letter == '7')
-    {
-        counter7++;
-    }
-    else if (letter == '8')
-    {
-        counter8++;
+    public string Login { get; private set; }
 
-    }
-    else if (letter == '9')
+    public string Password { get; private set; }
+
+    public void AddScore(int socre)
     {
-        counter9++;
+
     }
 }
-    Console.WriteLine("Wyniki dla liczby " + number);
-    Console.WriteLine("0 =>" + counter0);
-    Console.WriteLine("1 =>" + counter1);
-    Console.WriteLine("2 =>" + counter2);
-    Console.WriteLine("3 =>" + counter3);
-    Console.WriteLine("4 =>" + counter4);
-    Console.WriteLine("5 =>" + counter5);
-    Console.WriteLine("6 =>" + counter6);
-    Console.WriteLine("7 =>" + counter7);
-    Console.WriteLine("8 =>" + counter8);
-    Console.WriteLine("9 =>" + counter9);
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
