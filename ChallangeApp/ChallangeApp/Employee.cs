d26cd7de08cc07646818e91ab11edf8e8b270c74
Admin <woronczukprzemek@gmail.com>
2024-01-29 13:58:43 +0100

    zadanie dzień 6

---
+++ ChallangeApp/ChallangeApp/Employee.cs
@@ -0,0 +1,43 @@
+﻿
+
+
+namespace ChallangeApp
+{
+
+    public class Employee
+    {
+
+
+        private List<int> score = new List<int>();
+        public Employee(string name, string surename, int age)
+        {
+            this.Name = name;
+            this.SureName = surename;
+            this.Age = age;
+        }
+        public string Name { get; private set; }
+
+        public string SureName { get; private set; }
+
+        public int Age { get; private set; }
+
+        public string Score { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+        public void AddScore(int newScore)
+        {
+            this.score.Add(newScore);
+        }
+
+
+
+    }
+
+}
+
--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,28 +1,43 @@
-﻿User user1 = new User("Adam", "12345");
-User user2 = new User("Monika", "12345");
-User user3 = new User("Zuzia", "12345");
-User user4 = new User("Damian", "12345");
+﻿using ChallangeApp;
 
+Employee employee1 = new Employee("Adam", "Kowalski",25);
+Employee employee2 = new Employee("Monika", "Kowalska",56);
+Employee employee3 = new Employee("Zuzia", "Kowalska",54);
 
-user1.AddScore(5);
+employee1.AddScore(1);
+employee1.AddScore(2);
+employee1.AddScore(3);
 
-class User
+employee2.AddScore(4);
+employee2.AddScore(5);
+employee2.AddScore(6);
+
+employee3.AddScore(7);
+employee3.AddScore(8);
+employee3.AddScore(9);
+
+List<Employee> employees = new List<Employee>()
 {
-    private int score;
-  
+    employee1, employee2, employee3
+};
 
-    public User(string login, string password)
+int maxResult = -1;
+
+Employee employeeWithMaxResult = null;
+
+foreach (var employee in employees)
+{
+    if(employee.Result > maxResult)
     {
-        this.Login = login;
-        this.Password = password;
-        this.score = 0;
-    }
-    public string Login { get; private set; }
-
-    public string Password { get; private set; }
-
-    public void AddScore(int socre)
-    {
-
+        employeeWithMaxResult = employee;
+        maxResult = employee.Result;
+        
     }
 }
+
+Console.WriteLine("Użytkownik z największą liczbą ocen to:" + employeeWithMaxResult.Name + " " + employeeWithMaxResult.SureName + employeeWithMaxResult.Age);
+
+
+
+
+
