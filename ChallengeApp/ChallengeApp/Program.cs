94746f093a9aea3501a1a77a1804acf2dc62f658
Admin <woronczukprzemek@gmail.com>
2024-06-03 02:22:30 +0200

    zadanie domowe dzien 7

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,17 +1,13 @@
 ﻿using ChallengeApp;
 
-Employee user1 = new Employee("Adam", "Kowalski" , 25);
-Employee user2 = new Employee("Damian", "Wiak", 23);
-Employee user3 = new Employee("Darek", "Musiał", 21);
+User user1 = new User("Adam", "Kowalski" );
+User user2 = new User("Damian", "Wiak" );
+User user3 = new User("Darek", "Musiał");
 
 user1.AddScore(5);
 user1.AddScore(9);
 user1.AddScore(7);
-user1.AddScore(6);
-user1.AddScore(2);
 
-user2.AddScore(9);
-user2.AddScore(9);
 user2.AddScore(9);
 user2.AddScore(9);
 user2.AddScore(9);
@@ -19,33 +15,6 @@ user2.AddScore(9);
 user3.AddScore(1);
 user3.AddScore(9);
 user3.AddScore(7);
-user3.AddScore(7);
-user3.AddScore(8);
-
-List<Employee> users = new List<Employee>()
-{
-user1 ,user2 , user3
-
-};
-
-int maxResult = -1;
-
-Employee userWithMaxResult = null;
-
-foreach (var user in users)
-{
-    if(user.Result > maxResult)
-    {
-        userWithMaxResult = user;
-        maxResult = user.Result;
-    }
-}
-
-Console.WriteLine("Najlepszy Pracownik:");
-Console.WriteLine("Imię : " + userWithMaxResult.Name);
-Console.WriteLine("Nazwisko : " + userWithMaxResult.Surname);
-Console.WriteLine("Wiek : " + userWithMaxResult.Age);
-Console.WriteLine("Wynik : " + userWithMaxResult.Result);
 
 
 
---
+++ ChallengeApp/ChallengeApp/User.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System.Runtime.InteropServices;
+
+namespace ChallengeApp
+{
+   
+    public class User
+    {
+        private List<int> score = new List<int>();
+        public User(string login, string password)
+        {
+            this.Login = login;
+            this.Password = password;
+
+        }
+
+
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+        public void AddScore(int number)
+        {
+
+            this.score.Add(number);
+        }
+    }
+}
