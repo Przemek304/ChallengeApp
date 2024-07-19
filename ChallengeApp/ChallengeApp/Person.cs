a3edd053f32370c2c7b707a6b6c4a76e4fdcf909
Admin <przemekworonczuk@gmail.com>
2024-07-20 00:52:45 +0200

    zapis

---
+++ ChallengeApp/ChallengeApp/Person.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace ćwiczenia
+{
+    public abstract class Person
+    {
+        public Person(string name)
+        {
+            this.Name = name;
+           
+        }
+         public string Name { get;  private set; }
+        
+
+    }
+}
